69e413c2297e31919ee20b1f856f64cb788eb43b
Viktor Nagy <viktor.nagy@365.bank>
2023-05-27 11:51:23 +0200

    feat: configuration using array

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn luminance_percentil(r: u8, g: u8, b: u8) -> f32 {
     (0.2126 * r as f32 + 0.7152 * g as f32 + 0.0722 * b as f32) / 255.0
 }
 
+const REPLACEMENTS: &[char] = &['.', ',', 'i', 'o', 'b', 'H', 'G', '@', '&', '#'];
+
 fn main() {
     let img = image::open("resources/image.jpg").unwrap();
 
@@ -17,27 +19,9 @@ fn main() {
         for x in 0..dimensions.0 {
             let pixel = img.get_pixel(x, y);
             let lum = luminance_percentil(pixel[0], pixel[1], pixel[2]);
-            if lum < 0.1 {
-                result.push('.');
-            } else if lum < 0.2 {
-                result.push(',');
-            } else if lum < 0.3 {
-                result.push('i');
-            } else if lum < 0.4 {
-                result.push('o');
-            } else if lum < 0.5 {
-                result.push('b');
-            } else if lum < 0.6 {
-                result.push('H');
-            } else if lum < 0.7 {
-                result.push('G');
-            } else if lum < 0.8 {
-                result.push('@');
-            } else if lum < 0.9 {
-                result.push('&');
-            } else {
-                result.push('#');
-            }
+
+            let index = (lum * 10.0 - 1.0) as usize;
+            result.push(REPLACEMENTS[index]);
         }
         result.push('\n');
     }
